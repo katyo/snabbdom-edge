a8711d7b4705a687cf6f76b9621731184797f503
Kayo Phoenix <kayo@illumium.org>
2017-01-17 23:30:06 +0500

    Added main module

---
+++ src/snabbdom-edge.ts
@@ -0,0 +1,5 @@
+import {init as initDOMReader, Reader as DOMReader} from './read-dom';
+import {init as initHTMLWriter, Writer as HTMLWriter} from './write-html';
+
+export { initDOMReader, DOMReader };
+export { initHTMLWriter, HTMLWriter };
