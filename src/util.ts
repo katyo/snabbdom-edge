05391416a83e6b8cb23c4afb7a32326e8c5adf29
Kayo Phoenix <kayo@illumium.org>
2017-01-18 13:25:25 +0500

    Moved boolean attribute regex to util

---
+++ src/util.ts
@@ -0,0 +1,15 @@
+/* Boolean attributes regexp generator (elisp)
+   You can edit list of properties and re-evaluate expression
+
+   (replace-regexp "\\(booleanAttrsRegex = \\)[^;]*;"
+   (concat "\\1/^" (replace-regexp-in-string "\\\\" "" (regexp-opt '(
+   "allowfullscreen" "async" "autofocus" "autoplay" "checked" "compact" "controls" "declare"
+   "default" "defaultchecked" "defaultmuted" "defaultselected" "defer" "disabled" "draggable"
+   "enabled" "formnovalidate" "hidden" "indeterminate" "inert" "ismap" "itemscope" "loop" "multiple"
+   "muted" "nohref" "noresize" "noshade" "novalidate" "nowrap" "open" "pauseonexit" "readonly"
+   "required" "reversed" "scoped" "seamless" "selected" "sortable" "spellcheck" "translate"
+   "truespeed" "typemustmatch" "visible"
+   ))) "$/;"))
+*/
+
+export const booleanAttrsRegex = /^(?:a(?:llowfullscreen|sync|uto(?:focus|play))|c(?:hecked|o(?:mpact|ntrols))|d(?:e(?:clare|f(?:ault(?:(?:check|(?:mu|selec)t)ed)?|er))|isabled|raggable)|enabled|formnovalidate|hidden|i(?:n(?:determinate|ert)|smap|temscope)|loop|mu(?:ltiple|ted)|no(?:href|resize|shade|validate|wrap)|open|pauseonexit|re(?:adonly|(?:quir|vers)ed)|s(?:coped|e(?:amless|lected)|ortable|pellcheck)|t(?:r(?:anslate|uespeed)|ypemustmatch)|visible)$/;
